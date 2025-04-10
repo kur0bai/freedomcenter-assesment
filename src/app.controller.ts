3863b061d02a32000e5a6bb1b2c14762ec3ed43d
Jesus Salcedo <jesalceg@gmail.com>
2025-04-10 09:13:50 -0500

    refactor: adjust the jwt guard to catch authorization by token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,12 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/guards/jwt.guard';
-import { RolesGuard } from './auth/guards/roles.guard';
-import { Roles } from './auth/decorators/roles.decorator';
-import { Role } from './common/enums/role.enum';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.Admin)
-  @Get('admin')
-  adminRoute(@Request() req) {
-    return { message: 'Solo admin', user: req.user };
-  }
+  constructor(private readonly appService: AppService) {}
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  userRoute(@Request() req) {
-    return { message: 'Perfil', user: req.user };
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Get,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Role } from 'src/common/enums/role.enum';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { RolesGuard } from './guards/roles.guard';
+import { Roles } from './decorators/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +24,18 @@ export class AuthController {
   @Post('login')
   login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get('admin')
+  adminRoute(@Request() req) {
+    return { message: 'Solo admin', user: req.user };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  userRoute(@Request() req) {
+    return { message: 'Perfil', user: req.user };
   }
 }
